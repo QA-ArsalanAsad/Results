1607cff38f8a571a9b1a531a39572fc8d545ba04
Arsalan Asad <https://gitlab.com/arsalanasad96/python-pre-learning-tasks-published/-/tree/master>
2021-01-06 17:35:49 +0000

    results

---
+++ src/com/qa/main/Result.java
@@ -0,0 +1,22 @@
+package com.qa.main;
+
+public class Result {
+ static int biology=80;
+ static int chemistry=75;
+ static int physics=55;
+ 
+ int total=450;
+ 
+ public static int setBiology() {
+	 return biology;
+ }
+ 
+ public static int setChemistry() {
+	 return chemistry;
+ }
+
+ public static int setPhysics() {
+	 return physics;
+ }
+ 
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,22 @@
+package com.qa.main;
+
+
+public class Runner {
+	
+	static double num1=450;
+	static double num2=210;
+	static double percentage=(double) (num2/num1);
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("biology"+ 80);
+		System.out.println("chemistry" + 75);
+		System.out.println("physics" + 55);
+		System.out.println(percentage*100);
+
+		
+		
+	}
+
+}
